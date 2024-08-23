5facf4bd860cbc524fac0be5bfb15213b02b50f0
diarietoudiop <diarietou07diop@gmail.com>
2024-08-23 08:42:31 +0000

    dev-diary-commit

--- src/controllers/PostController.ts
+++ src/controllers/PostController.ts
@@ -1,13 +1,32 @@
 import { Request, Response } from 'express';
+
+// Your middleware code here
 import prisma from '../prisma';
+import { PrismaClient, Post, User } from '@prisma/client';
+
+
+
 interface Comment {
-    id: string; // Utilisez 'id' au lieu de '_id' pour Prisma
-    text: string;
-    commenterId: string;
-    updatedAt?: Date;
-  }
-  
-  
+  id: string;
+  text: string;
+  commenterId: string;
+  updatedAt?: Date;
+}
+
+interface CommentWithCommenter {
+  id: number;
+  content: string;
+  createdAt: Date;
+  updatedAt: Date;
+  userId: number;
+  postId: number;
+  commenter: {
+    nom: string;
+    prenom: string;
+  };
+}
+
+
   
   interface Viewer {
       id: string;
@@ -20,13 +39,12 @@ interface Comment {
       viewersIds: Viewer[];
       // ... autres propriétés du post si nécessaire
     }
-export default class PostController {
-
+export default class PostController { 
     static async createPost(req: Request, res: Response): Promise<void> {
         const { contenu, contenuMedia, type } = req.body;
     
         try {
-            const userId = req.user?.id ? parseInt(req.user.id, 10) : null;
+            const userId = req.user?.id ? parseInt(req.user?.id, 10) : null;
     
             if (!userId) {
                 res.status(401).send("Unauthorized");
@@ -81,7 +99,6 @@ export default class PostController {
     }
     
       
-
     static async deleteStoryExpire(): Promise<void> {
         try {
             const now = new Date();
@@ -100,208 +117,277 @@ export default class PostController {
 
 
 
-
-  static async addComment(req: Request, res: Response) {
-    const { postId } = req.params;
-    const { text } = req.body;
-    const commenterId = req.user?.id;
-
-    try {
-      if (!commenterId) {
-        return res.status(400).send('Commenter ID is missing');
+    static async addComment(req: Request, res: Response) {
+      const { postId } = req.params;
+      const { text } = req.body;
+      const userIdString = req.user?.id;
+    
+      if (!userIdString) {
+        return res.status(400).send('User ID is missing');
       }
-
-      const post = await prisma.post.findUnique({
-        where: { id: postId },
-        include: { comments: true }
-      });
-
-      if (!post) {
-        return res.status(404).send('Post not found');
+    
+      const userId = parseInt(userIdString, 10);
+    
+      if (isNaN(userId)) {
+        return res.status(400).send('Invalid User ID');
       }
-
-      if (!text || !text.trim()) {
-        return res.status(400).json({ message: "Le texte du commentaire ne peut pas être vide" });
-      }
-
-      const updatedPost = await prisma.post.update({
-        where: { id: postId },
-        data: {
-          comments: {
-            create: { text, commenterId }
+    
+      try {
+        if (!text || !text.trim()) {
+          return res.status(400).json({ message: "Le texte du commentaire ne peut pas être vide" });
+        }
+    
+        // Création du nouveau commentaire
+        const newComment = await prisma.comment.create({
+          data: {
+            content: text.trim(),
+            userId, // Maintenant c'est un nombre
+            postId: parseInt(postId, 10)
           }
-        },
-        include: { comments: true }
-      });
-
-      res.json(updatedPost.comments);
-    } catch (err) {
-      console.error(err);
-      res.status(500).send('Server Error');
+        });
+    
+        res.json(newComment);
+      } catch (err) {
+        console.error(err);
+        res.status(500).send('Server Error');
+      }
     }
+    
+    
+    
+    static async getComments(req: Request, res: Response) {
+      const { postId } = req.params;
+  
+      try {
+        const comments = await prisma.comment.findMany({
+          where: { postId: parseInt(postId, 10) },
+          include: {
+            
+          },
+        });
+        
+  
+          if (!comments.length) {
+              return res.status(404).send('No comments found for this post');
+          }
+  
+          res.json(comments);
+      } catch (err) {
+          console.error(err);
+          res.status(500).send('Server Error');
+      }
   }
+  
+  
+    
 
 
-  static async getComments(req: Request, res: Response) {
-    const { postId } = req.params;
-    const userId = req.user?.id; // L'ID de l'utilisateur connecté
 
-    try {
-      const post = await prisma.post.findUnique({
-        where: { id: postId },
-        include: {
-          comments: {
-            include: {
-              commenter: {
-                select: {
-                  nom: true,
-                  prenom: true
-                }
-              }
+
+      static async updateComment(req: Request, res: Response) {
+        const { postId, commentId } = req.params;
+        const { content } = req.body; 
+      
+        if (!commentId) {
+          return res.status(400).send('Comment ID is missing');
+        }
+      
+        try {
+          const updatedComment = await prisma.comment.update({
+            where: {
+              id: parseInt(commentId, 10), // Convertir commentId en nombre
+              postId: parseInt(postId, 10) // Convertir postId en nombre
+            },
+            data: {
+              content,
+              updatedAt: new Date()
             }
+          });
+      
+          if (!updatedComment) {
+            return res.status(404).send('Comment not found');
           }
+      
+          res.json(updatedComment);
+        } catch (err) {
+          console.error(err);
+          res.status(500).send('Server Error');
+        }  
+      }
+    
+
+
+
+
+      static async deleteComment(req: Request, res: Response): Promise<void> {
+        const { postId, commentId } = req.params;
+      
+        try {
+          // Trouver le post avec les commentaires associés
+          const post = await prisma.post.findUnique({
+            where: { id: parseInt(postId, 10) },
+            include: { comment: true }, // Changé de 'comments' à 'comment'
+          });
+      
+          if (!post) {
+            res.status(404).json({ message: 'Post not found' });
+            return;
+          }
+      
+          // Vérifier si le commentaire existe
+          const comment = await prisma.comment.findUnique({
+            where: { 
+              id: parseInt(commentId, 10),
+              postId: parseInt(postId, 10)
+            },
+          });
+      
+          if (!comment) {
+            res.status(404).json({ message: 'Comment not found' });
+            return;
+          }
+      
+          // Supprimer le commentaire
+          await prisma.comment.delete({
+            where: { id: comment.id },
+          });
+      
+          res.json({ message: 'Comment deleted' });
+        } catch (err) {
+          console.error(err);
+          res.status(500).json({ message: 'Server Error' });
         }
-      });
-
-      if (!post) {
-        return res.status(404).send('Post not found');
       }
 
-      res.json({
-        connectedUserId: userId,
-        comments: post.comments
-      });
-    } catch (err) {
-      console.error(err);
-      res.status(500).send('Server Error');
-    }
-  }
+      
 
-  static async deleteComment(req: Request, res: Response) {
-    const { postId, commentId } = req.params;
 
-    if (!commentId) {
-      return res.status(400).send('Comment ID is missing');
-    }
-
-    try {
-      const deletedComment = await prisma.comment.delete({
-        where: {
-          id: commentId,
-          postId: postId
+      static async incrementViews(req: Request, res: Response): Promise<void> {
+        const postId = req.params.postId;
+        const userId = req.user?.id;
+    
+        if (!userId) {
+          res.status(400).send('User ID is missing');
+          return;
         }
-      });
-
-      if (!deletedComment) {
-        return res.status(404).send('Comment not found');
-      }
-
-      res.json({ message: 'Comment deleted' });
-    } catch (err) {
-      console.error(err);
-      res.status(500).send('Server Error');
-    }
-  }
-
-  static async updateComment(req: Request, res: Response) {
-    const { postId, commentId } = req.params;
-    const { text } = req.body;
-
-    if (!commentId) {
-      return res.status(400).send('Comment ID is missing');
-    }
-
-    try {
-      const updatedComment = await prisma.comment.update({
-        where: {
-          id: commentId,
-          postId: postId
-        },
-        data: {
-          text,
-          updatedAt: new Date()
+    
+        try {
+          // Vérifiez que l'utilisateur existe
+          const user = await prisma.user.findUnique({
+            where: { id: parseInt(userId, 10) }
+          });
+    
+          if (!user) {
+            res.status(404).send('User not found');
+            return;
+          }
+    
+          // Vérifiez que le post existe
+          const post = await prisma.post.findUnique({
+            where: { id: parseInt(postId, 10) },
+            include: { viewers: true }
+          });
+    
+          if (!post) {
+            res.status(404).send('Post not found');
+            return;
+          }
+    
+          // Vérifiez si l'utilisateur a déjà vu le post
+          const viewerExists = post.viewers.some(viewer => viewer.userId === parseInt(userId, 10));
+    
+          if (viewerExists) {
+            // Si l'utilisateur a déjà vu le post, renvoyez le nombre de vues
+            res.json({ views: post.viewers.length });
+            return;
+          }
+    
+          // Ajoutez l'utilisateur comme vue du post
+          const updatedPost = await prisma.post.update({
+            where: { id: parseInt(postId, 10) },
+            data: {
+              viewers: {
+                create: { userId: parseInt(userId, 10) }
+              }
+            },
+            include: { viewers: true }
+          });
+          // Répondez avec le nombre de vues mises à jour
+          res.json({ views: updatedPost.viewers.length });
+        } catch (err) {
+          console.error('Server Error:', err);
+          res.status(500).send('Server Error');
         }
-      });
-
-      if (!updatedComment) {
-        return res.status(404).send('Comment not found');
       }
-
-      res.json(updatedComment);
-    } catch (err) {
-      console.error(err);
-      res.status(500).send('Server Error');
-    }
-  }
-
-
-  static async incrementViews(req: Request, res: Response) {
-    const { postId, userId } = req.params;
-
-    if (!userId) {
-      return res.status(400).send('User ID is missing');
-    }
-
-    try {
-      const user = await prisma.user.findUnique({
-        where: { id: userId }
-      });
-
-      if (!user) {
-        return res.status(404).send('User not found');
-      }
-
-      const post = await prisma.post.findUnique({
-        where: { id: postId },
-        include: { viewersIds: true }
-      });
-
-      if (!post) {
-        return res.status(404).send('Post not found');
-      }
-
-      // Utilisez l'interface Viewer pour typer correctement le paramètre 'viewer'
-      if (post.viewersIds.some((viewer: Viewer) => viewer.id === userId)) {
-        return res.json({ views: post.views });
-      }
-
-      const updatedPost = await prisma.post.update({
-        where: { id: postId },
-        data: {
-          views: { increment: 1 },
-          viewersIds: { connect: { id: userId } }
+           
+      
+      // static async incrementViews(req: Request, res: Response): Promise<void> {
+      //   // const { postId, userId } = req.params;
+      //   const { postId } = req.params;
+      //   const userId = req.user?.id;
+        
+      //   try {
+          
+      //     // Trouver l'utilisateur
+      //     const user = await userId?.findById(userId);
+      //     if (!user) {
+      //       res.status(404).send('User not found');
+      //       return;
+      //     }
+    
+      //     // Trouver le post
+      //     const post = await Post.findById(postId);
+      //     if (!post) {
+      //       res.status(404).send('Post not found');
+      //       return;
+      //     }
+    
+      //     // Vérifier si l'utilisateur a déjà visionné le post
+      //     if (post.viewersIds.includes(userId)) {
+      //       res.json({ views: post.views });
+      //       return;
+      //     }
+    
+      //     // Incrémenter le nombre de vues et ajouter l'ID de l'utilisateur
+      //     const updatedPost = await Post.findByIdAndUpdate(
+      //       postId,
+      //       { $inc: { views: 1 }, $addToSet: { viewersIds: userId } },
+      //       { new: true }
+      //     );
+    
+      //     if (updatedPost) {
+      //       res.json({ views: updatedPost.views });
+      //     } else {
+      //       res.status(404).send('Post not found');
+      //     }
+      //   } catch (err) {
+      //     console.error(err.message);
+      //     res.status(500).send('Server Error');
+      //   }
+      // }
+    
+      static async getViews(req: Request, res: Response) {
+        const { postId } = req.params;
+      
+        try {
+          const post = await prisma.post.findUnique({
+            where: { id: parseInt(postId, 10) },
+            include: { viewers: true }
+          });
+      
+          if (!post) {
+            return res.status(404).send('Post not found');
+          }
+      
+          res.json({
+            views: post.viewers.length,
+            viewerIds: post.viewers.map((viewer: { id: number }) => viewer.id)
+          });
+        } catch (err) {
+          console.error(err);
+          res.status(500).send('Server Error');
         }
-      });
-
-      res.json({ views: updatedPost.views });
-    } catch (err) {
-      console.error(err);
-      res.status(500).send('Server Error');
-    }
-  }
-
-  static async getViews(req: Request, res: Response) {
-    const { postId } = req.params;
-
-    try {
-      const post = await prisma.post.findUnique({
-        where: { id: postId },
-        include: { viewersIds: true }
-      }) as PostWithViewers | null;
-
-      if (!post) {
-        return res.status(404).send('Post not found');
       }
-
-      res.json({
-        views: post.views,
-        viewersIds: post.viewersIds.map((viewer: Viewer) => viewer.id)
-      });
-    } catch (err) {
-      console.error(err);
-      res.status(500).send('Server Error');
-    }
-  }
 
   // ... Adaptez les autres méthodes de la même manière ...
 }
--- src/routes/PostRoute.ts
+++ src/routes/PostRoute.ts
@@ -7,33 +7,33 @@ const router:Router =  express.Router();
 // creer un post les variable requis { contenu, contenuMedia }
 
 router.post('/create',Middleware.verifyToken,Middleware.validateData("post"), Middleware.canPost,PostController.createPost);
-// //modifier un post les variable requis { contenu, contenuMedia }
+//modifier un post les variable requis { contenu, contenuMedia }
 // router.put('/:id',Middleware.verifyToken,Middleware.validateData("post"), PostController.modifyPost);
-// //recuperer tous les status 
+//recuperer tous les status 
 // router.get('/status', PostController.getAllStatus);
-// //delete a post
+//delete a post
 // router.delete('/:id',Middleware.verifyToken, PostController.deletePost);
-// //commentaire
-// //ajouter un commentaire les variable requis 
-// // const { postId, commentId } = req.params;
-// //     const { text } = req.body;
-// router.post('/:postId/comment', Middleware.verifyToken, validateData('comment'), PostController.addComment);
-// //recuperer tous les commentaires d'un post 
-// router.get('/:postId/comment', PostController.getComments);
-// //modifier un commentaire  
-// // const { postId, commentId } = req.params;
-// //     const { text } = req.body;
-// router.put('/:postId/comment/:commentId',Middleware.validateData('comment'), PostController.updateComment);
-// //supprimer un commentaire 
-// router.delete('/:postId/comment/:commentId', PostController.deleteComment);
-// //story 
+//commentaire
+//ajouter un commentaire les variable requis 
+// const { postId, commentId } = req.params;
+//     const { text } = req.body;
+router.post('/:postId/comment', Middleware.verifyToken, /* validateData('comment'), */ PostController.addComment);
+//recuperer tous les commentaires d'un post 
+router.get('/:postId/comment', PostController.getComments);
+//modifier un commentaire  
+// const { postId, commentId } = req.params;
+//     const { text } = req.body;
+router.put('/:postId/comment/:commentId',Middleware.validateData('comment'), PostController.updateComment);
+// supprimer un commentaire 
+router.delete('/:postId/comment/:commentId', PostController.deleteComment);
+//story 
 // router.post('/createStory',Middleware.verifyToken,Middleware.validateData("post"),Middleware.canPost, PostController.createStory);
 
 
 // // Nouvelles routes pour les vues 
-// router.get('/:postId/view',Middleware.verifyToken, PostController.incrementViews);
-// //voilr les vue d'un post 
-// router.get('/:postId/views',Middleware.verifyToken, PostController.getViews);
+router.post('/:postId/view',Middleware.verifyToken, PostController.incrementViews);
+//voilr les vue d'un post 
+router.get('/:postId/views',Middleware.verifyToken, PostController.getViews);
 // //file actu 
 // router.get('/accueil',Middleware.verifyToken,PostController.fileActu)
 
